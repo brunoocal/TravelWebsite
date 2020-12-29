e9b0a54134c0edecefc6a8285ea7a6b183592c33
Bruno Gabriel Cal Olivera <75707587+brunoocal@users.noreply.github.com>
2020-12-29 16:48:02 -0300

    Minor change

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,6 +1,16 @@
 let progress = document.getElementById('progressbar');
+let progressbackground = document.getElementById('progressbackground');
 window.onscroll = function(){
     let totalHeight = document.body.scrollHeight - window.innerHeight;
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    
+    if(progressHeight > 40){
+        progressbackground.style.background = "rgba(0, 0, 0, 0.6)";
+        progress.style.background = "black";
+    }else{
+        progressbackground.style.background = "rgba(255, 255, 255, 0.2)"
+        progress.style.background = "white";
+    }
+
     progress.style.height = progressHeight + "%";
 }
